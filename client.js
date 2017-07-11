b863bc153225a090e45d124a894f70c52d0f039c
Jeffrey Wang <j@mywikis.com>
2017-07-11 02:11:05 -0500

    Completely rewritten with Web Speech Synthesis API

--- client.js
+++ client.js
@@ -4,32 +4,65 @@
 
 // ------- Data ------- \\
 
+var ss = window.speechSynthesis;
 
-var sounds = {
+// var sounds = {
+//   general: {
+//     nextstop: new Howl({
+//       src: ['assets/nextstop.mp3']
+//     }),
+//     bus: new Howl({
+//       src: ['assets/bus.mp3']
+//     }),
+//     arriving: new Howl({
+//       src: ['assets/arriving.mp3']
+//     }),
+//     welcome: new Howl({
+//       src: ['assets/welcome.mp3']
+//     })
+//   },
+//   dp: {
+//     dp: new Howl({
+//       src: ['assets/dp.mp3']
+//     }),
+//     gab: new Howl({
+//       src: ['assets/dp-gab.mp3']
+//     }),
+//     aveg: new Howl({
+//       src: ['assets/dp-aveg.mp3']
+//     })
+//   }
+// };
+
+var speech = {
   general: {
-    nextstop: new Howl({
-      src: ['assets/nextstop.mp3']
-    }),
-    bus: new Howl({
-      src: ['assets/bus.mp3']
-    }),
-    arriving: new Howl({
-      src: ['assets/arriving.mp3']
-    }),
-    welcome: new Howl({
-      src: ['assets/welcome.mp3']
-    })
+    nextstop: 'Next stop',
+    thisstop: 'You are currently at',
+    bus: 'bus',
+    arriving: 'Now arriving at',
+    welcome: 'Welcome to the',
+    nb: 'northbound',
+    sb: 'southbound',
+    nt: 'northern terminus',
+    st: 'southern terminus',
+    thankyou: 'Thank you for riding the',
+    thisis: 'This is the',
+    fullstop: '.'
+  },
+  dcta: {
+    thankyou: 'Thank you for riding with DCTA, the Denton County Transportation Authority.'
   },
   dp: {
-    dp: new Howl({
-      src: ['assets/dp.mp3']
-    }),
-    gab: new Howl({
-      src: ['assets/dp-gab.mp3']
-    }),
-    aveg: new Howl({
-      src: ['assets/dp-aveg.mp3']
-    })
+    dp: 'Discovery Park',
+    gab: 'the GAB',
+    aveg: 'Avenue G',
+    emery: 'Emery',
+    university: 'University',
+    windsor: 'Windsor',
+    aveh: 'Avenue H',
+    stella: 'Stella at North Texas',
+    rec: 'Rec Center',
+    eesat: 'Environmental Science Building'
   }
 };
 
@@ -40,34 +73,77 @@ var sounds = {
 // ------- Functions ------ \\
 
 function play( category, name ) {
-  sounds[category][name].play();
+  // sounds[category][name].play();
+  var utterance = new SpeechSynthesisUtterance( speech[category][name] );
+  ss.speak(utterance);
+}
+
+function playall( arr ) {
+  var uttertext = '';
+  arr.forEach(function(element) {
+    var category = element[0];
+    var name = element[1];
+    uttertext += speech[category][name];
+  });
+  var utterance = new SpeechSynthesisUtterance( uttertext );
+  ss.speak(utterance);
 }
 
 // ------- jQuery ------- \\
 
 $("#announcements").on('click', '.welcome', function( event ) {
-  // var idinfo = $(this).attr('id').split('-');
-  // var from = idinfo[1];
-  var sound = sounds['general']['welcome'];
-  sound.once('load', function(){
-    sound.play();
-  });
-  sound.on('end', function(){
-    var sound2 = sounds['dp']['dp'];
-    sound2.play();
-    sound2.on('end', function(){
-      sounds['general']['bus'].play();
-    })
-  });
+  event.preventDefault();
+  // sounds['general']['welcome'].play();
+  // sounds['dp']['dp'].play();
+  // sounds['general']['bus'].play();
+  // play( 'general', 'welcome' );
+  // play( 'dp', 'dp' );
+  // play( 'general', 'bus' );
+  playall( [['general', 'welcome'], ['dp', 'dp'], ['general', 'bus'], ['general', 'fullstop']] );
+});
+
+$("#announcements").on('click', '.thankyou', function( event ) {
+  event.preventDefault();
+  // sounds['general']['welcome'].play();
+  // sounds['dp']['dp'].play();
+  // sounds['general']['bus'].play();
+  // play( 'general', 'thankyou' );
+  // play( 'dp', 'dp' );
+  // play( 'general', 'bus' );
+  playall( [['general', 'thankyou'], ['dp', 'dp'], ['general', 'bus']] );
+  play( 'dcta', 'thankyou' );
 });
 
 $("#announcements").on('click', '.nextstop', function( event ) {
+  event.preventDefault();
   var idinfo = $(this).attr('id').split('-');
-  var sound = sounds['general']['nextstop'];
-  sound.play();
+  // sounds['general']['nextstop'].play();
+  // sounds[idinfo[0]][idinfo[1]].play();
+  // play( 'general', 'thisis' );
+  // play( 'dp', 'dp' );
+  // play( 'general', 'bus' );
+  playall( [['general', 'thisis'], ['dp', 'dp'], ['general', 'bus'], ['general', 'fullstop']] );
+  play( 'general', 'nextstop' );
+  play( idinfo[1], idinfo[2] );
+  play( 'general', idinfo[3] );
+});
 
-  // Fires when the sound finishes playing.
-  sound.on('end', function(){
-    sounds[idinfo[0]][idinfo[1]].play();
-  });
+$("#announcements").on('click', '.thisstop', function( event ) {
+  event.preventDefault();
+  var idinfo = $(this).attr('id').split('-');
+  // sounds['general']['nextstop'].play();
+  // sounds[idinfo[0]][idinfo[1]].play();
+  play( 'general', 'thisstop' );
+  play( idinfo[1], idinfo[2] );
+  play( 'general', idinfo[3] );
+});
+
+$("#announcements").on('click', '.arriving', function( event ) {
+  event.preventDefault();
+  var idinfo = $(this).attr('id').split('-');
+  // sounds['general']['nextstop'].play();
+  // sounds[idinfo[0]][idinfo[1]].play();
+  play( 'general', 'arriving' );
+  play( idinfo[1], idinfo[2] );
+  play( 'general', idinfo[3] );
 });
